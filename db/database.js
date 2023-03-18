0146d6f0aa6c3f9d116da981223b7ae2c4d3a87e
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-18 14:14:23 +0100

    connecting to mongodb via mongoose

--- db/database.js
+++ db/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Method 1 using async await
 const mongodbConnection = async (MONGODB_URL) => {
   mongoose.set("strictQuery", false);
 
@@ -11,5 +12,20 @@ const mongodbConnection = async (MONGODB_URL) => {
     console.log(error);
   }
 };
+
+// Method 2 using promises
+// const mongodbConnection = (MONGODB_URL) => {
+//   mongoose.set("strictQuery", false);
+
+//   mongoose
+//     .connect(MONGODB_URL)
+//     .then(() => {
+//       console.log("Database connection established.");
+//     })
+//     .catch((error) => {
+//       console.log("Failed to connect to database.");
+//       console.log(error);
+//     });
+// };
 
 module.exports = mongodbConnection;
