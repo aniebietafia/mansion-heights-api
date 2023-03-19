e80dae8d5e8ef6d7095cfed3d32fe81d61630417
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-19 21:09:32 +0100

    adding express session middleware

--- app.js
+++ app.js
@@ -7,15 +7,36 @@ require("dotenv").config();
 const morgan = require("morgan");
 const chalk = require("chalk");
 // const debug = require("debug")("app");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
 
 // Invoking express app
 const app = express();
 
 // middlewares
 app.use(morgan("tiny"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore({
+      checkPeriod: 86400000,
+    }),
+    cookie: {
+      secure: true,
+      maxAge: 1000 * 60 * 10,
+    },
+  })
+);
+
+require("./config/passport")(app);
 
 // Setting up the ejs
 app.set("view engine", "ejs");
@@ -25,7 +46,7 @@ app.set("views", "views");
 
 // Importing custom modules and mongodb
 const mongodbConnection = require("./db/database");
-const authUserRoute = require("./routes/client.routes");
+const authUserRoute = require("./routes/authUser.routes");
 
 // app middlewares
 app.use("/mansion-heights", authUserRoute);
