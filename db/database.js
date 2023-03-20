2332ad8bf5ba0ebdbd2aa5e615740e1a907fab79
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-20 18:09:57 +0100

    added database name

--- db/database.js
+++ db/database.js
@@ -2,33 +2,35 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Method 1 using async await
-const mongodbConnection = async (MONGODB_URL) => {
-  mongoose.set("strictQuery", false);
-
-  try {
-    await mongoose.connect(MONGODB_URL, {
-      dbName: process.env.dbName,
-    });
-    console.log("Database connection established");
-  } catch (error) {
-    console.log("Failed to connect to database");
-    console.log(error);
-  }
-};
-
-// Method 2 using promises
-// const mongodbConnection = (MONGODB_URL) => {
+// const mongodbConnection = async (MONGODB_URL) => {
 //   mongoose.set("strictQuery", false);
 
-//   mongoose
-//     .connect(MONGODB_URL)
-//     .then(() => {
-//       console.log("Database connection established.");
-//     })
-//     .catch((error) => {
-//       console.log("Failed to connect to database.");
-//       console.log(error);
+//   try {
+//     await mongoose.connect(MONGODB_URL, {
+//       dbName: process.env.dbName,
 //     });
+//     console.log("Database connection established");
+//   } catch (error) {
+//     console.log("Failed to connect to database");
+//     console.log(error);
+//   }
 // };
+
+// Method 2 using promises
+const mongodbConnection = (MONGODB_URL) => {
+  mongoose.set("strictQuery", false);
+
+  mongoose
+    .connect(MONGODB_URL, {
+      dbName: process.env.dbName,
+    })
+    .then((result) => {
+      console.log("Database connection established.");
+    })
+    .catch((error) => {
+      console.log("Failed to connect to database.");
+      console.log(error);
+    });
+};
 
 module.exports = mongodbConnection;
