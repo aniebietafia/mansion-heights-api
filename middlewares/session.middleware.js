119e7317bf3f018d3ffa9a2665f0207e08c6b4d9
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-20 18:11:13 +0100

    adding express session middleware

---
+++ middlewares/session.middleware.js
@@ -0,0 +1,11 @@
+const sessionConfig = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+module.exports = sessionConfig;
