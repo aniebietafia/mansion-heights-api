62464c5152d73bfc67d866305766c37a87ae1552
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-21 22:00:08 +0100

    adding password hashing and compare password middleware

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema(
   {
@@ -42,5 +43,10 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// UserSchema.pre("save", async function () {
+//   const salt = await bcrypt.genSalt(12);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
 
 module.exports = mongoose.model("User", UserSchema);
