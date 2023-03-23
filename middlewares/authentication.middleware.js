e9c7876c9d885649bdb72b1e0d4751ff243b6fa2
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-23 19:02:52 +0100

    authentication middleware

---
+++ middlewares/authentication.middleware.js
@@ -0,0 +1,31 @@
+const CustomError = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.signedCookies.token;
+
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication failed");
+  }
+  try {
+    const { fullName, userId, role } = isTokenValid({ token });
+    req.user = { fullName, userId, role };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication failed");
+  }
+};
+
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError("You're not authorized to access this resource.");
+    }
+    next();
+  };
+};
+
+module.exports = {
+  authenticateUser,
+  authorizePermissions,
+};
