54549e1b0bda915515b27ac310e3efa1b8f655d9
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-23 19:14:51 +0100

    setting up property routes

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const authenticateUser = require("../middlewares/authentication.middleware");
 const propertyController = require("../controllers/property.controller");
 
-router.route("/").get(propertyController.getAllProperties).post(propertyController.postProperty);
+router.route("/").get(propertyController.getAllProperties);
+
+router.route("/property").get(propertyController.getPropertyRegisterForm).post(propertyController.postProperty);
 
 router
   .route("/:id")
