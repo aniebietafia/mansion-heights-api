0bd5cebd9d85a8850cefc56ad45f4a9bfd9dfa8b
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-28 10:22:22 +0100

    fixed the landing page

--- routes/authUser.routes.js
+++ routes/authUser.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const authUserController = require("../controllers/authUser.controller");
 
-router.route("/").get(authUserController.indexPage);
-
 router.route("/signup").get(authUserController.getUserSignUpForm).post(authUserController.postUserSignUp);
 
 router.route("/login").get(authUserController.getLoginForm).post(authUserController.postUserLogin);
