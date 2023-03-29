559877b519f41b2dd666c2aad0cc29785c219281
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-29 08:36:06 +0100

    functionality for user signup form validation

---
+++ public/js/signup.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const password = document.getElementById("password");
+
+const processFormData = () => {
+  console.log("Start");
+};
+
+// Event Listener
+form.addEventListener("submit", processFormData);
