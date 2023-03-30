e3f788d875c7ddafe3ab19c22183c568162e87d8
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-30 18:01:14 +0100

    creating and verifying tokens

--- utils/jwt.js
+++ utils/jwt.js
@@ -9,11 +9,13 @@ const isTokenValid = ({ token }) => {
   return jwt.verify(token, process.env.SECRET);
 };
 
+// function to attach cookies to the response
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
 
   const thirtyDays = 1000 * 60 * 60 * 24 * 30;
 
+  // adding jwt token to the response of a created user
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + thirtyDays),
