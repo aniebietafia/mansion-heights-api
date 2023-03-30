44e26d42c67a7c3ab756ddbba08ed66a80d91a41
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-30 18:00:13 +0100

    adding frontend validation to the signup form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,24 @@
 const form = document.querySelector("form");
 const password = document.getElementById("password");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
 
-const processFormData = () => {
-  console.log("Start");
+let isValid = false;
+
+const validateForm = () => {
+  // Using contraint API
+  isValid = form.checkValidity();
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+  }
+};
+
+const processFormData = (e) => {
+  //   e.preventDefault();
+  //   Validate Form
+  validateForm();
 };
 
 // Event Listener
