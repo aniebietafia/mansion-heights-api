d9348d2df39258f17164aecb4484cbf4efbf2cb2
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-03 19:49:13 +0100

    utility function to check for user permissions

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -3,7 +3,7 @@ const CustomError = require("../errors");
 const checkPermissions = (requestUser, requestUserId) => {
   if (requestUser.role === "admin") return;
   if (requestUser.userId === requestUserId.toString()) return;
-  throw new CustomError.UnauthorizedError("You're not authorized to access this resource");
+  throw new CustomError.UnauthorizedError("You're not authorized to do that");
 };
 
 module.exports = checkPermissions;
