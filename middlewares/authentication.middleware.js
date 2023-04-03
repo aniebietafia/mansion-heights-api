e97001aa128ee88dba71683c6806a33c185b58c3
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-03 19:41:29 +0100

    middleware to handle authentication and authorization of users

--- middlewares/authentication.middleware.js
+++ middlewares/authentication.middleware.js
@@ -25,11 +25,6 @@ const authorizePermissions = (...roles) => {
   };
 };
 
-// const authorizePermissions = (req, res, next) => {
-//   console.log("admin route");
-//   next();
-// };
-
 module.exports = {
   authenticateUser,
   authorizePermissions,
