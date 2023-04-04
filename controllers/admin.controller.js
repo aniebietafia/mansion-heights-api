9a764cd9a72b4c1689a126ae6bbfefd1ae26186f
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-04 20:00:02 +0100

    admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,12 +1,10 @@
-const Apartment = require("../models/property.models");
 const User = require("../models/user.models");
-const CustomError = require("../errors");
-const { checkPermissions } = require("../utils/index");
 
 // Fetch all users in database
 const getAllUsers = async (req, res) => {
   const users = await User.find({});
   res.render("admin/users", {
+    userObj: req.user,
     pageTitle: "All Users",
     users,
   });
@@ -18,8 +16,42 @@ const adminEditLodgeForm = async (req, res) => {
     pageTitle: "Edit Lodge",
   });
 };
+const editUserForm = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.render("admin/edit-user", {
+    userObj: req.user,
+    pageTitle: "Edit User",
+    user,
+  });
+};
+
+const postEditedUser = async (req, res) => {
+  const { fullName, email, tel_number, gender, role } = req.body;
+  const user = await User.findById(req.params.id);
+
+  user.fullName = fullName;
+  user.email = email;
+  user.tel_number = tel_number;
+  user.gender = gender;
+  user.role = role;
+
+  await user.save();
+  res.redirect("/lodge-finder/admin/users");
+};
+
+const deleteUser = async (req, res) => {
+  const { id: userId } = req.params;
+  const user = await User.findOne({ _id: userId });
+
+  await user.deleteOne();
+
+  res.redirect("/lodge-finder/admin/users");
+};
 
 module.exports = {
   getAllUsers,
   adminEditLodgeForm,
+  deleteUser,
+  editUserForm,
+  postEditedUser,
 };
