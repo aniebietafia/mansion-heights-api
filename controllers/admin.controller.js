0f895044e4539597a1f89b8e18bd397fddc7cc19
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-05 22:43:05 +0100

    admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -10,14 +10,17 @@ const getAllUsers = async (req, res) => {
   });
 };
 
+// get admin form to edit an apartment
 const adminEditLodgeForm = async (req, res) => {
   res.render("admin/edit", {
     userObj: req.user,
     pageTitle: "Edit Lodge",
   });
 };
+
+// render the form update users in the database
 const editUserForm = async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).select("-password");
   res.render("admin/edit-user", {
     userObj: req.user,
     pageTitle: "Edit User",
@@ -25,10 +28,14 @@ const editUserForm = async (req, res) => {
   });
 };
 
+// controller to update user in the database
 const postEditedUser = async (req, res) => {
+  const { id } = req.params;
   const { fullName, email, tel_number, gender, role } = req.body;
-  const user = await User.findById(req.params.id);
+  const user = await User.findOne({ _id: id });
+  console.log(user);
 
+  // updating old data with the new data
   user.fullName = fullName;
   user.email = email;
   user.tel_number = tel_number;
@@ -39,11 +46,13 @@ const postEditedUser = async (req, res) => {
   res.redirect("/lodge-finder/admin/users");
 };
 
+// controller to delete user(s) from the database
 const deleteUser = async (req, res) => {
+  // fetching the user by id
   const { id: userId } = req.params;
-  const user = await User.findOne({ _id: userId });
+  const user = await User.findByIdAndDelete({ _id: userId });
 
-  await user.deleteOne();
+  // await user.deleteOne();
 
   res.redirect("/lodge-finder/admin/users");
 };
