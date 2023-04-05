549270e1d1814a02445cb6b91f4875fc2302644c
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-05 22:46:57 +0100

    authenticate user route

--- routes/authUser.routes.js
+++ routes/authUser.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const authUserController = require("../controllers/authUser.controller");
 
 router.route("/signup").get(authUserController.getUserSignUpForm).post(authUserController.postUserSignUp);
-
 router.route("/login").get(authUserController.getLoginForm).post(authUserController.postUserLogin);
-
 router.route("/logout").post(authUserController.logout);
+
+router.route("/reset").get(authUserController.resetPasswordForm).post(authUserController.resetPassword);
+router.route("/reset/:token").get(authUserController.newPasswordForm);
+router.route("/new-password").post(authUserController.postNewPassword);
 
 module.exports = router;
