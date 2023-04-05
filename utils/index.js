53f9b583d5733b77653655c831b5b70d18240a19
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-05 22:50:37 +0100

    exporting utility functions

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,8 @@ const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
 const { createTokenUser } = require("./createTokenUser");
 const checkPermissions = require("./checkPermissions");
 const { capitalizeFullName, lowerCaseEmail, convertFeaturesToArray } = require("./algorithms");
+const sendResetPasswordEmail = require("./sendResetPasswordEmail");
+const createHash = require("./createHash");
 
 module.exports = {
   createJWT,
@@ -12,4 +14,6 @@ module.exports = {
   capitalizeFullName,
   lowerCaseEmail,
   convertFeaturesToArray,
+  sendResetPasswordEmail,
+  createHash,
 };
